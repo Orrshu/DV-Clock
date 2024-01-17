ad750fffb03ef66e8f6587b8a2a871e2d2982ff3
Orrshu <156861104+Orrshu@users.noreply.github.com>
2024-01-17 09:11:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,52 +1,62 @@
 let secondDrops = [];
-let minuteDrops = [];
-let oceanLevel = 0;
+let minuteOceanLevel = 0;
+let hourOceanLevel = 0;
+let lastSecond = -1;
+let lastMinute = -1;
+let lastHour = -1;
 
 function setup() {
     createCanvas(800, 600);
-}
+    lastHour = hour();
+    lastMinute = minute();
+    lastSecond = second();
 
+    hourOceanLevel = map(lastHour, 0, 23, 0, height); 
+    minuteOceanLevel = map(lastMinute, 0, 59, 0, 50); 
+}
 
 function draw() {
     background(225);
 
-    let isAM = hour() < 12;
-    let oceanColor = isAM ? color(0, 150, 255) : color(0, 71, 171); // Light blue for AM, dark blue for PM
+    let currentSecond = second();
+    let currentMinute = minute();
+    let currentHour = hour();
+    let isAM = currentHour < 12;
 
-    // Update and draw second drops
-    if (frameCount % 60 == 0) { // Every second
+    let minuteOceanColor = isAM ? color(137, 207, 240) : color(0, 0, 139);
+    let hourOceanColor = isAM ? color(100, 149, 237) : color(25, 25, 112);
+
+    if (currentSecond !== lastSecond) {
         secondDrops.push(new Drop(random(width), 0, color(0, 0, 255), 5));
+        lastSecond = currentSecond;
     }
     for (let i = secondDrops.length - 1; i >= 0; i--) {
         secondDrops[i].fall();
         secondDrops[i].show();
-        if (secondDrops[i].y > height - oceanLevel) {
+        if (secondDrops[i].y > height - minuteOceanLevel) {
             secondDrops.splice(i, 1);
         }
     }
 
-    if (frameCount % 3600 == 0) { // Every minute
-        let currentMinute = minute();
-        let minSize = map(currentMinute, 0, 59, 10, 20); // Size changes with minute
-        let minColor = color(137, 207, 240); // Constant color for all minute drops
-        minuteDrops.push(new Drop(random(width), 0, minColor, minSize));
+    if (currentMinute !== lastMinute) {
+        minuteOceanLevel = map(currentMinute, 0, 59, 0, 50);
+        lastMinute = currentMinute;
     }
-    for (let i = minuteDrops.length - 1; i >= 0; i--) {
-        minuteDrops[i].fall();
-        minuteDrops[i].show();
-        if (minuteDrops[i].y > height - oceanLevel) {
-            minuteDrops.splice(i, 1);
-        }
-    }
-    // Update ocean level based on hours
-    oceanLevel = map(hour() % 12, 0, 12, 0, height / 2);
 
-    // Draw ocean
-    fill(oceanColor, 150); // semi-transparent
-    rect(0, height - oceanLevel, width, oceanLevel);
+    if (currentHour !== lastHour) {
+        hourOceanLevel = map(currentHour, 0, 23, 0, height);
+        lastHour = currentHour;
     }
-    
-    // Drop class
+
+   fill(hourOceanColor);
+   rect(0, height - hourOceanLevel, width, hourOceanLevel);
+
+   let minuteOceanY = height - hourOceanLevel - minuteOceanLevel; // Adjust Y position of minute ocean
+    fill(minuteOceanColor);
+    rect(0, minuteOceanY, width, minuteOceanLevel);
+
+}
+
     class Drop {
     constructor(x, y, col, size) {
     this.x = x;
@@ -66,5 +76,3 @@ function draw() {
         ellipse(this.x, this.y, this.size);
     }
 }
-
-
